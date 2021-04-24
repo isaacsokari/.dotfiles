55b964076ac0a8b1171e5f9aecf88061fbaed515
Isaac Sokari <isaacsokari@gmail.com>
2021-04-24 19:29:45 +0100

    initialized dotfiles repo

---
+++ .vimrc
@@ -0,0 +1,59 @@
+" :W sudo saves the file when the file is open in readonly mode
+command W w !sudo tee % > /dev/null
+
+""""""""""""""""""""""""""""""""""""
+" Line
+""""""""""""""""""""""""""""""""""""
+" show line numbers
+set number
+
+"""""""""""""""""""""""""""""""""""""
+" Indents
+""""""""""""""""""""""""""""""""""""
+" replace tabs with spaces
+set expandtab
+
+" 1 tab = 2 spaces
+set tabstop=2 shiftwidth=2
+
+" when deleting whitespace at the beginning of a line, delete 
+" 1 tab worth of spaces (for us this is 2 spaces)
+set smarttab
+
+" when creating a new line, copy the indentation from the line above
+set autoindent
+
+"""""""""""""""""""""""""""""""""""""
+" Search
+"""""""""""""""""""""""""""""""""""""
+" Ignore case when searching
+
+set ignorecase
+set smartcase
+
+" " highlight search results (after pressing Enter)
+set hlsearch
+
+" " highlight all pattern matches WHILE typing the pattern
+set incsearch
+
+"""""""""""""""""""""""""""""""""""""
+" Mix
+"""""""""""""""""""""""""""""""""""""
+" show the mathing brackets
+
+set showmatch
+
+"
+" highlight current line
+set cursorline
+
+" space open/closes folds
+nnoremap <space> za
+
+" fold based on indent level
+set foldmethod=indent   
+
+" enable modelines for file-specific settings
+" 1 means it'll be at the bottom of the file
+set modelines=1
---
+++ install.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd `dirname $0` && pwd)
+DOTFILES_DIR="$HOME/.dotfiles"
+
+# ------------------------------------------------------------------------------------------
+# Link and Back up Helper
+# ------------------------------------------------------------------------------------------
+
+_link_and_backup() {
+  DEFAULT_FILE="$HOME/$1"
+  LINK_FILE="$SCRIPT_DIR/$1"
+
+  if [[ ! -L "$DEFAULT_FILE" ]]; then
+    mv "$DEFAULT_FILE" "$DEFAULT_FILE.backup"
+  else
+    LINKING_TO=$(readlink "$DEFAULT_FILE")
+
+    if [[ "$LINKING_TO" == "$LINK_FILE" ]]; then
+      echo "> Symlink for $DEFAULT_FILE already exists. Skipping!"
+      return
+    else
+      mv "$DEFAULT_FILE" "$DEFAULT_FILE.backup"
+    fi
+  fi
+
+  ln -s "$LINK_FILE" "$DEFAULT_FILE"
+  echo "> Created symlink for $DEFAULT_FILE."
+}
+
+
+# ------------------------------------------------------------------------------------------
+# Begin Script
+# ------------------------------------------------------------------------------------------
+
+echo "======================================================================="
+echo "Beginning installation"
+echo "======================================================================="
+echo ""
+
+cd $HOME
+
+# Move the dotfiles directory to the HOME path if necessary...
+[[ ! -d $DOTFILES_DIR ]] && mv $SCRIPT_DIR $DOTFILES_DIR
+
+_link_and_backup ".curlrc"
+_link_and_backup ".viminfo"
+_link_and_backup ".zshrc"
+_link_and_backup ".wgetrc"
+_link_and_backup ".hushlogin"
+# _link_and_backup ".screenrc"
+_link_and_backup ".gitconfig"
+# _link_and_backup ".gitconfig.work"
+# _link_and_backup ".bashrc"
+_link_and_backup ".vimrc"
+
+echo ""
+echo "======================================================================="
+echo "Installation complete"
+echo "======================================================================="
