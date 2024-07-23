30ab17165e98e52ee35a7e5bf5bdac3c63ce94b0
Isaac Sokari <isaacsokari@gmail.com>
2024-07-23 16:37:27 +0100

    chore: remove nvim-spectre

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -53,7 +53,7 @@ return {
         { "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
         { "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
         { "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
-        { "<leader>sr", "<cmd>Telescope resume<cr>", desc = "Resume" },
+        -- { "<leader>sr", "<cmd>Telescope resume<cr>", desc = "Resume" },
         -- { "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
         { "<leader>sw", Util.pick("grep_string", { word_match = "-w" }), desc = "Word (root dir)" },
         { "<leader>sW", Util.pick("grep_string", { cwd = nil, word_match = "-w" }), desc = "Word (cwd)" },
