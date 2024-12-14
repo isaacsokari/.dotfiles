6e85e513c58daa768fdaee065893648d09023a9b
Isaac Sokari <isaacsokari@gmail.com>
2024-12-14 19:13:01 +0100

    feat(nvim_kick): add snacks dashboard

--- config/nvim_kick/lua/plugins/ui/ui.lua
+++ config/nvim_kick/lua/plugins/ui/ui.lua
@@ -3,149 +3,151 @@ local lsp_symbols = require("ts.config.lsp_symbols")
 
 ---@return {get_fg?:string}?
 local function get_fg(name)
-  local hl = vim.api.nvim_get_hl(0, { name = name, link = false })
-  ---@diagnostic disable-next-line: undefined-field
-  local fg = hl and hl.fg or hl.foreground
-  return fg and { fg = string.format("#%06x", fg) } or nil
+	local hl = vim.api.nvim_get_hl(0, { name = name, link = false })
+	---@diagnostic disable-next-line: undefined-field
+	local fg = hl and hl.fg or hl.foreground
+	return fg and { fg = string.format("#%06x", fg) } or nil
 end
 
 return {
-  -- bufferline - tabs for buffers with include filetype icons and close buttons.
-  {
-    "akinsho/bufferline.nvim",
-    event = "VeryLazy",
-    keys = {
-      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>",            desc = "Toggle Pin" },
-      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
-      { "<leader>br", "<Cmd>BufferLineCloseRight<CR>",           desc = "Delete Buffers to the Right" },
-      { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>",            desc = "Delete Buffers to the Left" },
-      { "<S-h>",      "<cmd>BufferLineCyclePrev<cr>",            desc = "Prev Buffer" },
-      { "<S-l>",      "<cmd>BufferLineCycleNext<cr>",            desc = "Next Buffer" },
-      { "[b",         "<cmd>BufferLineCyclePrev<cr>",            desc = "Prev Buffer" },
-      { "]b",         "<cmd>BufferLineCycleNext<cr>",            desc = "Next Buffer" },
-      { "[B",         "<cmd>BufferLineMovePrev<cr>",             desc = "Move buffer prev" },
-      { "]B",         "<cmd>BufferLineMoveNext<cr>",             desc = "Move buffer next" },
-    },
-    opts = {
-      options = {
+	-- bufferline - tabs for buffers with include filetype icons and close buttons.
+	{
+		"akinsho/bufferline.nvim",
+		event = "VeryLazy",
+		keys = {
+			{ "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+			{ "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+			{ "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+			{ "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+			{ "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+			{ "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+			{ "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+			{ "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+			{ "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+			{ "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+		},
+		opts = {
+			options = {
         -- stylua: ignore
         close_command = function(n) require("snacks").bufdelete(n) end,
         -- stylua: ignore
         right_mouse_command = function(n) require("snacks").bufdelete(n) end,
-        diagnostics = "nvim_lsp",
-        always_show_bufferline = false,
-        diagnostics_indicator = function(_, _, diag)
-          local diagnostics_icons = icons.diagnostics
-          local ret = (diag.error and diagnostics_icons.Error .. diag.error .. " " or "")
-              .. (diag.warning and diagnostics_icons.Warn .. diag.warning or "")
-          return vim.trim(ret)
-        end,
-        offsets = {
-          {
-            filetype = "neo-tree",
-            text = "Neo-tree",
-            highlight = "Directory",
-            text_align = "left",
-          },
-        },
-        ---@param opts bufferline.IconFetcherOpts
-        get_element_icon = function(opts)
-          return icons.ft[opts.filetype]
-        end,
-      },
-    },
-    config = function(_, opts)
-      require("bufferline").setup(opts)
-      -- Fix bufferline when restoring a session
-      vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
-        callback = function()
-          vim.schedule(function()
-            pcall(nvim_bufferline)
-          end)
-        end,
-      })
-    end,
-  },
+				diagnostics = "nvim_lsp",
+				always_show_bufferline = false,
+				diagnostics_indicator = function(_, _, diag)
+					local diagnostics_icons = icons.diagnostics
+					local ret = (diag.error and diagnostics_icons.Error .. diag.error .. " " or "")
+						.. (diag.warning and diagnostics_icons.Warn .. diag.warning or "")
+					return vim.trim(ret)
+				end,
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "Neo-tree",
+						highlight = "Directory",
+						text_align = "left",
+					},
+				},
+				---@param opts bufferline.IconFetcherOpts
+				get_element_icon = function(opts)
+					return icons.ft[opts.filetype]
+				end,
+			},
+		},
+		config = function(_, opts)
+			require("bufferline").setup(opts)
+			-- Fix bufferline when restoring a session
+			vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
+				callback = function()
+					vim.schedule(function()
+						pcall(nvim_bufferline)
+					end)
+				end,
+			})
+		end,
+	},
 
-  -- indent guides for Neovim
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    opts = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('snacks').toggle({
-        name = "Indention Guides",
-        get = function()
-          return require("ibl.config").get_config(0).enabled
-        end,
-        set = function(state)
-          require("ibl").setup_buffer(0, { enabled = state })
-        end,
-      }):map("<leader>ug")
+	-- indent guides for Neovim
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		opts = function()
+			---@diagnostic disable-next-line: missing-fields
+			require("snacks")
+				.toggle({
+					name = "Indention Guides",
+					get = function()
+						return require("ibl.config").get_config(0).enabled
+					end,
+					set = function(state)
+						require("ibl").setup_buffer(0, { enabled = state })
+					end,
+				})
+				:map("<leader>ug")
 
-      return {
-        indent = {
-          char = "│",
-          tab_char = "│",
-        },
-        scope = { show_start = false, show_end = false },
-        exclude = {
-          filetypes = {
-            "Trouble",
-            "alpha",
-            "dashboard",
-            "help",
-            "lazy",
-            "mason",
-            "neo-tree",
-            "notify",
-            "snacks_dashboard",
-            "snacks_notif",
-            "snacks_terminal",
-            "snacks_win",
-            "toggleterm",
-            "trouble",
-          },
-        },
-      }
-    end,
-    main = "ibl",
-  },
+			return {
+				indent = {
+					char = "│",
+					tab_char = "│",
+				},
+				scope = { show_start = false, show_end = false },
+				exclude = {
+					filetypes = {
+						"Trouble",
+						"alpha",
+						"dashboard",
+						"help",
+						"lazy",
+						"mason",
+						"neo-tree",
+						"notify",
+						"snacks_dashboard",
+						"snacks_notif",
+						"snacks_terminal",
+						"snacks_win",
+						"toggleterm",
+						"trouble",
+					},
+				},
+			}
+		end,
+		main = "ibl",
+	},
 
-  -- messages, cmdline and the popupmenu
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      -- ui components
-      { "MunifTanjim/nui.nvim", lazy = true },
-    },
-    opts = {
-      lsp = {
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
-      routes = {
-        {
-          filter = {
-            event = "msg_show",
-            any = {
-              { find = "%d+L, %d+B" },
-              { find = "; after #%d+" },
-              { find = "; before #%d+" },
-            },
-          },
-          view = "mini",
-        },
-      },
-      presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-      },
-    },
+	-- messages, cmdline and the popupmenu
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			-- ui components
+			{ "MunifTanjim/nui.nvim", lazy = true },
+		},
+		opts = {
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+			},
+			routes = {
+				{
+					filter = {
+						event = "msg_show",
+						any = {
+							{ find = "%d+L, %d+B" },
+							{ find = "; after #%d+" },
+							{ find = "; before #%d+" },
+						},
+					},
+					view = "mini",
+				},
+			},
+			presets = {
+				bottom_search = true,
+				command_palette = true,
+				long_message_to_split = true,
+			},
+		},
     -- stylua: ignore
     keys = {
       { "<leader>sn",  "",                                                                            desc = "+noice" },
@@ -158,120 +160,83 @@ return {
       { "<c-f>",       function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end,  silent = true,                           expr = true,              desc = "Scroll Forward",  mode = { "i", "n", "s" } },
       { "<c-b>",       function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true,                           expr = true,              desc = "Scroll Backward", mode = { "i", "n", "s" } },
     },
-    config = function(_, opts)
-      -- HACK: noice shows messages from before it was enabled,
-      -- but this is not ideal when Lazy is installing plugins,
-      -- so clear the messages in this case.
-      if vim.o.filetype == "lazy" then
-        vim.cmd([[messages clear]])
-      end
-      require("noice").setup(opts)
-    end,
-  },
+		config = function(_, opts)
+			-- HACK: noice shows messages from before it was enabled,
+			-- but this is not ideal when Lazy is installing plugins,
+			-- so clear the messages in this case.
+			if vim.o.filetype == "lazy" then
+				vim.cmd([[messages clear]])
+			end
+			require("noice").setup(opts)
+		end,
+	},
 
-  {
-    "rcarriga/nvim-notify",
-    opts = {
-      timeout = 5000,
-    },
-  },
+	{
+		"rcarriga/nvim-notify",
+		opts = {
+			timeout = 5000,
+		},
+	},
 
-  {
-    "folke/zen-mode.nvim",
-    cmd = "ZenMode",
-    opts = {
-      plugins = {
-        gitsigns = true,
-        tmux = true,
-        kitty = { enabled = false, font = "+2" },
-      },
-    },
-    keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
-  },
+	{
+		"folke/zen-mode.nvim",
+		cmd = "ZenMode",
+		opts = {
+			plugins = {
+				gitsigns = true,
+				tmux = true,
+				kitty = { enabled = false, font = "+2" },
+			},
+		},
+		keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
+	},
 
-  --   {
-  --     "folke/snacks.nvim",
-  --     opts = {
-  --       dashboard = {
-  --         preset = {
-  --           -- generated the logo using
-  --           -- https://patorjk.com/software/taag/#p=display&h=0&f=ANSI%20Shadow&t=TS
-  --           header = [[
-  -- ████████╗███████╗
-  -- ╚══██╔══╝██╔════╝
-  --    ██║   ███████╗
-  --    ██║   ╚════██║
-  --    ██║   ███████║
-  --    ╚═╝   ╚══════╝
-  --       ]],
-  --         },
-  --
-  --         keys = {
-  --           { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-  --           { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-  --           { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-  --           { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-  --           {
-  --             icon = " ",
-  --             key = "c",
-  --             desc = "Config",
-  --             action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
-  --           },
-  --           { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-  --           { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
-  --           { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
-  --           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-  --         }
-  --       },
-  --     },
-  --   },
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.g.lualine_laststatus = vim.o.laststatus
+			if vim.fn.argc(-1) > 0 then
+				-- set an empty statusline till lualine loads
+				vim.o.statusline = " "
+			else
+				-- hide the statusline on the starter page
+				vim.o.laststatus = 0
+			end
+		end,
+		opts = function()
+			-- PERF: we don't need this lualine require madness 🤷
+			local lualine_require = require("lualine_require")
+			lualine_require.require = require
 
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.g.lualine_laststatus = vim.o.laststatus
-      if vim.fn.argc(-1) > 0 then
-        -- set an empty statusline till lualine loads
-        vim.o.statusline = " "
-      else
-        -- hide the statusline on the starter page
-        vim.o.laststatus = 0
-      end
-    end,
-    opts = function()
-      -- PERF: we don't need this lualine require madness 🤷
-      local lualine_require = require("lualine_require")
-      lualine_require.require = require
+			vim.o.laststatus = vim.g.lualine_laststatus
 
-      vim.o.laststatus = vim.g.lualine_laststatus
+			return {
+				options = {
+					theme = "auto",
+					globalstatus = true,
+					disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
+					-- disable section separators
+					section_separators = "",
+					component_separators = "",
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch" },
 
-      return {
-        options = {
-          theme = "auto",
-          globalstatus = true,
-          disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
-          -- disable section separators
-          section_separators = "",
-          component_separators = "",
-        },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = { "branch" },
+					lualine_c = {
+						{
+							"diagnostics",
+							symbols = {
+								error = icons.diagnostics.Error,
+								warn = icons.diagnostics.Warn,
+								info = icons.diagnostics.Info,
+								hint = icons.diagnostics.Hint,
+							},
+						},
+					},
 
-          lualine_c = {
-            {
-              "diagnostics",
-              symbols = {
-                error = icons.diagnostics.Error,
-                warn = icons.diagnostics.Warn,
-                info = icons.diagnostics.Info,
-                hint = icons.diagnostics.Hint,
-              },
-            },
-          },
-
-          lualine_y = {
+					lualine_y = {
 
             -- stylua: ignore
             {
@@ -298,91 +263,92 @@ return {
               color = get_fg("Debug"),
             },
 
-            {
-              require("lazy.status").updates,
-              cond = require("lazy.status").has_updates,
-              color = get_fg("Special"),
-            },
+						{
+							require("lazy.status").updates,
+							cond = require("lazy.status").has_updates,
+							color = get_fg("Special"),
+						},
 
-            {
-              "diff",
-              symbols = {
-                added = icons.git.added,
-                modified = icons.git.modified,
-                removed = icons.git.removed,
-              },
-              source = function()
-                local gitsigns = vim.b.gitsigns_status_dict
-                if gitsigns then
-                  return {
-                    added = gitsigns.added,
-                    modified = gitsigns.changed,
-                    removed = gitsigns.removed,
-                  }
-                end
-              end,
-            },
-          },
+						{
+							"diff",
+							symbols = {
+								added = icons.git.added,
+								modified = icons.git.modified,
+								removed = icons.git.removed,
+							},
+							source = function()
+								local gitsigns = vim.b.gitsigns_status_dict
+								if gitsigns then
+									return {
+										added = gitsigns.added,
+										modified = gitsigns.changed,
+										removed = gitsigns.removed,
+									}
+								end
+							end,
+						},
+					},
 
-          lualine_z = {
-            { "progress", separator = " ",                  padding = { left = 1, right = 0 } },
-            { "location", padding = { left = 0, right = 1 } },
-          },
-        },
+					lualine_z = {
+						{ "progress", separator = " ", padding = { left = 1, right = 0 } },
+						{ "location", padding = { left = 0, right = 1 } },
+					},
+				},
 
         -- stylua: ignore
         extensions = { "lazy" },
-      }
-    end,
-  },
+			}
+		end,
+	},
 
-  -- icons
-  {
-    "echasnovski/mini.icons",
-    lazy = true,
-    opts = {
-      file = {
-        [".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
-        ["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
-      },
-      filetype = {
-        dotenv = { glyph = "", hl = "MiniIconsYellow" },
-      },
-    },
-    init = function()
-      package.preload["nvim-web-devicons"] = function()
-        require("mini.icons").mock_nvim_web_devicons()
-        return package.loaded["nvim-web-devicons"]
-      end
-    end,
-  },
+	-- icons
+	{
+		"echasnovski/mini.icons",
+		lazy = true,
+		opts = {
+			file = {
+				[".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
+				["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
+			},
+			filetype = {
+				dotenv = { glyph = "", hl = "MiniIconsYellow" },
+			},
+		},
+		init = function()
+			package.preload["nvim-web-devicons"] = function()
+				require("mini.icons").mock_nvim_web_devicons()
+				return package.loaded["nvim-web-devicons"]
+			end
+		end,
+	},
 
-  {
-    "hedyhli/outline.nvim",
-    keys = { { "<leader>cs", "<cmd>Outline<cr>", desc = "Toggle Outline" } },
-    cmd = "Outline",
-    opts = function()
-      local defaults = require("outline.config").defaults
-      local opts = {
-        symbols = {
-          icons = {},
-          filter = vim.deepcopy(lsp_symbols),
-        },
-        keymaps = {
-          up_and_jump = "<up>",
-          down_and_jump = "<down>",
-        },
-      }
+	-- symbol outline
+	{
+		"hedyhli/outline.nvim",
+		keys = { { "<leader>cs", "<cmd>Outline<cr>", desc = "Toggle Outline" } },
+		cmd = "Outline",
+		opts = function()
+			local defaults = require("outline.config").defaults
+			local opts = {
+				symbols = {
+					icons = {},
+					filter = vim.deepcopy(lsp_symbols),
+				},
+				keymaps = {
+					up_and_jump = "<up>",
+					down_and_jump = "<down>",
+				},
+			}
 
-      for kind, symbol in pairs(defaults.symbols.icons) do
-        local icon_kinds = require("ts.config.icons").kinds
-        ---@diagnostic disable-next-line: assign-type-mismatch
-        icon_kinds[kind] = {
-          icon = icon_kinds[kind] or symbol.icon,
-          hl = symbol.hl,
-        }
-      end
-      return opts
-    end,
-  },
+			for kind, symbol in pairs(defaults.symbols.icons) do
+				local icon_kinds = require("ts.config.icons").kinds
+				---@diagnostic disable-next-line: assign-type-mismatch
+				icon_kinds[kind] = {
+					icon = icon_kinds[kind] or symbol.icon,
+					hl = symbol.hl,
+				}
+			end
+			return opts
+		end,
+	},
 }
