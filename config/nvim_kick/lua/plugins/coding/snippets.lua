1585070ead828dfa8b584e53e2ec35653f703301
Isaac Sokari <isaacsokari@gmail.com>
2024-12-14 18:33:29 +0100

    wip(nvim_kick): add completions and snippets

--- config/nvim_kick/lua/plugins/coding/snippets.lua
+++ config/nvim_kick/lua/plugins/coding/snippets.lua
@@ -3,89 +3,93 @@ local common_utils = require("ts.utils.common")
 local cmp = {}
 
 ---@param snippet string
+---@diagnostic disable-next-line: undefined-doc-name
 ---@param fn fun(placeholder:Placeholder):string
 ---@return string
 function cmp.snippet_replace(snippet, fn)
-  return snippet:gsub("%$%b{}", function(m)
-    local n, name = m:match("^%${(%d+):(.+)}$")
-    return n and fn({ n = n, text = name }) or m
-  end) or snippet
+	return snippet:gsub("%$%b{}", function(m)
+		local n, name = m:match("^%${(%d+):(.+)}$")
+		return n and fn({ n = n, text = name }) or m
+	end) or snippet
 end
 
 -- This function resolves nested placeholders in a snippet.
 ---@param snippet string
 ---@return string
 function cmp.snippet_preview(snippet)
-  local ok, parsed = pcall(function()
-    return vim.lsp._snippet_grammar.parse(snippet)
-  end)
-  return ok and tostring(parsed)
-    or cmp
-      .snippet_replace(snippet, function(placeholder)
-        return cmp.snippet_preview(placeholder.text)
-      end)
-      :gsub("%$0", "")
+	local ok, parsed = pcall(function()
+		return vim.lsp._snippet_grammar.parse(snippet)
+	end)
+	return ok and tostring(parsed)
+		or cmp.snippet_replace(snippet, function(placeholder)
+			---@diagnostic disable-next-line: undefined-field
+			return cmp.snippet_preview(placeholder.text)
+		end):gsub("%$0", "")
 end
 
 -- This function replaces nested placeholders in a snippet with LSP placeholders.
 function cmp.snippet_fix(snippet)
-  local texts = {} ---@type table<number, string>
-  return cmp.snippet_replace(snippet, function(placeholder)
-    texts[placeholder.n] = texts[placeholder.n] or cmp.snippet_preview(placeholder.text)
-    return "${" .. placeholder.n .. ":" .. texts[placeholder.n] .. "}"
-  end)
+	local texts = {} ---@type table<number, string>
+	return cmp.snippet_replace(snippet, function(placeholder)
+		---@diagnostic disable-next-line: undefined-field
+		texts[placeholder.n] = texts[placeholder.n] or cmp.snippet_preview(placeholder.text)
+		---@diagnostic disable-next-line: undefined-field
+		return "${" .. placeholder.n .. ":" .. texts[placeholder.n] .. "}"
+	end)
 end
 
 function cmp.expand(snippet)
-  -- Native sessions don't support nested snippet sessions.
-  -- Always use the top-level session.
-  -- Otherwise, when on the first placeholder and selecting a new completion,
-  -- the nested session will be used instead of the top-level session.
-  -- See: https://github.com/LazyVim/LazyVim/issues/3199
-  local session = vim.snippet.active() and vim.snippet._session or nil
+	-- Native sessions don't support nested snippet sessions.
+	-- Always use the top-level session.
+	-- Otherwise, when on the first placeholder and selecting a new completion,
+	-- the nested session will be used instead of the top-level session.
+	-- See: https://github.com/LazyVim/LazyVim/issues/3199
+	local session = vim.snippet.active() and vim.snippet._session or nil
 
-  local ok, err = pcall(vim.snippet.expand, snippet)
-  if not ok then
-    local fixed = cmp.snippet_fix(snippet)
-    ok = pcall(vim.snippet.expand, fixed)
+	local ok, err = pcall(vim.snippet.expand, snippet)
+	if not ok then
+		local fixed = cmp.snippet_fix(snippet)
+		ok = pcall(vim.snippet.expand, fixed)
 
-    local msg = ok and "Failed to parse snippet,\nbut was able to fix it automatically."
-      or ("Failed to parse snippet.\n" .. err)
+		local msg = ok and "Failed to parse snippet,\nbut was able to fix it automatically."
+			or ("Failed to parse snippet.\n" .. err)
 
-    vim.notify(
-      ([[%s
+		vim.notify(
+			([[%s
 ```%s
 %s
 ```]]):format(msg, vim.bo.filetype, snippet),
-      ok and "warn" or "error",
-      { title = "vim.snippet" }
-    )
-  end
+			---@diagnostic disable-next-line: param-type-mismatch
+			ok and "warn" or "error",
+			{ title = "vim.snippet" }
+		)
+	end
 
-  -- Restore top-level session when needed
-  if session then
-    vim.snippet._session = session
-  end
+	-- Restore top-level session when needed
+	if session then
+		vim.snippet._session = session
+	end
 end
 
 return {
-  {
-    "L3MON4D3/LuaSnip",
-    build = (not jit.os:find("Windows"))
-        and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
-      or nil,
+	{
+		-- Snippet Engine & its associated nvim-cmp source
+		"L3MON4D3/LuaSnip",
+		build = (not jit.os:find("Windows"))
+				and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
+			or nil,
 
-    dependencies = {
-      "rafamadriz/friendly-snippets",
-      config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
-      end,
-    },
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			config = function()
+				require("luasnip.loaders.from_vscode").lazy_load()
+			end,
+		},
 
-    opts = {
-      history = true,
-      delete_check_events = "TextChanged",
-    },
+		opts = {
+			history = true,
+			delete_check_events = "TextChanged",
+		},
 
     -- stylua: ignore
     keys = {
@@ -106,30 +110,30 @@ return {
         end,
         mode = { "i", "s" } },
     },
-  },
+	},
 
-  {
-    "nvim-cmp",
-    optional = true,
-    dependencies = {
-      {
-        "garymjr/nvim-snippets",
-        opts = {
-          friendly_snippets = true,
-        },
-        dependencies = { "rafamadriz/friendly-snippets" },
-      },
-    },
-    opts = function(_, opts)
-      opts.snippet = {
-        expand = function(item)
-          return cmp.expand(item.body)
-        end,
-      }
+	{
+		"nvim-cmp",
+		optional = true,
+		dependencies = {
+			{
+				"garymjr/nvim-snippets",
+				opts = {
+					friendly_snippets = true,
+				},
+				dependencies = { "rafamadriz/friendly-snippets" },
+			},
+		},
+		opts = function(_, opts)
+			opts.snippet = {
+				expand = function(item)
+					return cmp.expand(item.body)
+				end,
+			}
 
-      if common_utils.has("nvim-snippets") then
-        table.insert(opts.sources, { name = "snippets" })
-      end
-    end,
-  },
+			if common_utils.has("nvim-snippets") then
+				table.insert(opts.sources, { name = "snippets" })
+			end
+		end,
+	},
 }
