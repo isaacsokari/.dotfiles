6fc4c2f1b65ffa83772e6b9a76ca461546c61a85
Isaac Sokari <isaacsokari@gmail.com>
2025-01-02 09:14:18 +0100

    chore(nvim): remove <leader>th keymap, use Snacks to toggle inlay_hints

--- config/nvim/lua/plugins/lsp/lsp.lua
+++ config/nvim/lua/plugins/lsp/lsp.lua
@@ -229,15 +229,15 @@ return {
 						end
 					end
 
-					-- The following code creates a keymap to toggle inlay hints in your
-					-- code, if the language server you are using supports them
-					--
-					-- This may be unwanted, since they displace some of your code
-					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-						map("<leader>th", function()
-							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
-						end, "[T]oggle Inlay [H]ints")
-					end
+					-- -- The following code creates a keymap to toggle inlay hints in your
+					-- -- code, if the language server you are using supports them
+					-- --
+					-- -- This may be unwanted, since they displace some of your code
+					-- if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+					-- 	map("<leader>th", function()
+					-- 		vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+					-- 	end, "[T]oggle Inlay [H]ints")
+					-- end
 
 					if client and common_utils.has("snacks") then
 						local hasWillRename = client.supports_method(vim.lsp.protocol.Methods.workspace_willRenameFiles)
