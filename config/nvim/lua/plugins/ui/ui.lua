27d5827ee3271bc6e0dd5725833aefd6a11b248f
Isaac Sokari <isaacsokari@gmail.com>
2025-01-04 00:35:35 +0100

    feat(nvim): remove bufferline.nvim

--- config/nvim/lua/plugins/ui/ui.lua
+++ config/nvim/lua/plugins/ui/ui.lua
@@ -1,4 +1,3 @@
-local icons = require("ts.config.icons")
 local lsp_symbols = require("ts.config.lsp_symbols")
 
 ---@return {get_fg?:string}?
@@ -10,66 +9,6 @@ local function get_fg(name)
 end
 
 return {
-	-- bufferline - tabs for buffers with include filetype icons and close buttons.
-	{
-		"akinsho/bufferline.nvim",
-		event = "VeryLazy",
-		keys = {
-			{ "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
-			{ "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
-			{ "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
-			{ "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
-			{ "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
-			{ "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
-			{ "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
-			{ "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
-			{ "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
-			{ "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
-		},
-		opts = {
-			options = {
-        -- stylua: ignore
-        close_command = function(n) require("snacks").bufdelete(n) end,
-        -- stylua: ignore
-        right_mouse_command = function(n) require("snacks").bufdelete(n) end,
-				diagnostics = "nvim_lsp",
-				always_show_bufferline = false,
-				diagnostics_indicator = function(_, _, diag)
-					-- local diagnostics_icons = icons.diagnostics
-					-- local ret = (diag.error and diagnostics_icons.Error .. diag.error .. " " or "")
-					-- 	.. (diag.warning and diagnostics_icons.Warn .. diag.warning or "")
-
-					local ret = (diag.error and "E:" .. diag.error .. " " or "")
-						.. (diag.warning and "W:" .. diag.warning or "")
-
-					return vim.trim(ret)
-				end,
-				offsets = {
-					{
-						filetype = "neo-tree",
-						text = "Neo-tree",
-						highlight = "Directory",
-						text_align = "left",
-					},
-				},
-				---@param opts bufferline.IconFetcherOpts
-				get_element_icon = function(opts)
-					return icons.ft[opts.filetype]
-				end,
-			},
-		},
-		config = function(_, opts)
-			require("bufferline").setup(opts)
-			-- Fix bufferline when restoring a session
-			vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
-				callback = function()
-					vim.schedule(function()
-						pcall(nvim_bufferline)
-					end)
-				end,
-			})
-		end,
-	},
 
 	-- indent guides for Neovim
 	{
