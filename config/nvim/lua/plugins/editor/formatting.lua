2276f5191d790945bc2876921f725b29e20845a1
Isaac Sokari <isaacsokari@gmail.com>
2025-01-07 10:51:34 +0100

    feat(nvim): (conform.nvim) fallback to lsp formatting by default

--- config/nvim/lua/plugins/editor/formatting.lua
+++ config/nvim/lua/plugins/editor/formatting.lua
@@ -20,23 +20,13 @@ return {
 		opts = {
 			notify_on_error = false,
 
-			-- -- format_on_save is managed by ts.utils.formatting
-			-- format_on_save = function(bufnr)
-			-- 	-- Disable "format_on_save lsp_fallback" for languages that don't
-			-- 	-- have a well standardized coding style. You can add additional
-			-- 	-- languages here or re-enable it for the disabled ones.
-			-- 	local disable_filetypes = { c = true, cpp = true }
-			-- 	local lsp_format_opt
-			-- 	if disable_filetypes[vim.bo[bufnr].filetype] then
-			-- 		lsp_format_opt = "never"
-			-- 	else
-			-- 		lsp_format_opt = "fallback"
-			-- 	end
-			-- 	return {
-			-- 		timeout_ms = 500,
-			-- 		lsp_format = lsp_format_opt,
-			-- 	}
-			-- end,
+			-- Set this to change the default values when calling conform.format()
+			-- This will also affect the default values for format_on_save/format_after_save
+			default_format_opts = {
+				lsp_format = "fallback",
+				timeout_ms = 500,
+				stop_after_first = true,
+			},
 
 			formatters_by_ft = {
 				lua = { "stylua" },
