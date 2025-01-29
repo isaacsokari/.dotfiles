6bf198496117f6c9f2a593850bf7dc7f9b616736
Isaac Sokari <isaacsokari@gmail.com>
2025-01-29 01:32:59 +0100

    chore: remove zen-mode and update lazy-lock.json

--- config/nvim/lua/plugins/ui/ui.lua
+++ config/nvim/lua/plugins/ui/ui.lua
@@ -122,19 +122,6 @@ return {
 	},
 
 	{
-		"folke/zen-mode.nvim",
-		cmd = "ZenMode",
-		opts = {
-			plugins = {
-				gitsigns = true,
-				tmux = true,
-				kitty = { enabled = false, font = "+2" },
-			},
-		},
-		keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
-	},
-
-	{
 		"nvim-lualine/lualine.nvim",
 		event = "VeryLazy",
 		init = function()
