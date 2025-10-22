f89013ed45c8f9fcb332b7421431d3a706a831db
Isaac Sokari <isaacsokari@gmail.com>
2025-10-22 14:03:29 +0100

    chore(nvim): update gopls settings, add analyses and semantic tokens support

--- config/nvim/lua/plugins/lsp/go.lua
+++ config/nvim/lua/plugins/lsp/go.lua
@@ -37,12 +37,38 @@ return {
 								parameterNames = true,
 								rangeVariableTypes = true,
 							},
+
+							analyses = {
+								nilness = true,
+								unusedparams = true,
+								unusedwrite = true,
+								useany = true,
+							},
 						},
 					},
 				},
 			},
 
-			setup = {},
+			setup = {
+				gopls = function(_, opts)
+					-- workaround for gopls not supporting semanticTokensProvider
+					-- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+					vim.lsp.on_attach(function(client, _)
+						if not client.server_capabilities.semanticTokensProvider then
+							local semantic = client.config.capabilities.textDocument.semanticTokens
+							client.server_capabilities.semanticTokensProvider = {
+								full = true,
+								legend = {
+									tokenTypes = semantic.tokenTypes,
+									tokenModifiers = semantic.tokenModifiers,
+								},
+								range = true,
+							}
+						end
+					end, "gopls")
+					-- end workaround
+				end,
+			},
 		},
 	},
 
