eeb0a7ec22e07823ba375e3c8dea402111aad8ee
Isaac Sokari <29305267+isaacsokari@users.noreply.github.com>
2026-07-29 20:16:53 +0100

    chore(nvim): remove coffee and slim plugins

--- config/nvim/lua/plugins/lsp/rails.lua
+++ config/nvim/lua/plugins/lsp/rails.lua
@@ -1,8 +1,4 @@
 return {
-	-- add slim syntax
-	{ "slim-template/vim-slim", ft = "slim" },
-	-- coffeescript
-	{ "kchmck/vim-coffee-script", ft = "coffee" },
 	-- rails
 	{ "tpope/vim-rails", ft = "ruby" },
 }
