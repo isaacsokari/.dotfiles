f19d18a4f036d7969a586a71072e3c102a83dc22
Isaac Sokari <29305267+isaacsokari@users.noreply.github.com>
2026-07-31 14:45:24 +0100

    feat(nvim): enable Emmet language server

--- config/nvim/lua/plugins/lsp/lsp.lua
+++ config/nvim/lua/plugins/lsp/lsp.lua
@@ -75,6 +75,7 @@ return {
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			servers = {
 				cssls = {},
+				emmet_language_server = {},
 
 				html = {},
 
